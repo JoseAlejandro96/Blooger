aefa58ea47b4cd763f431ddaf594b4d0aba0cae6
JoseAlejandro96 <jscuesta96@gmail.com>
2020-12-16 14:07:57 +0100

    final website

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items: 2
+    },
+    960:{
+        items: 3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -8,5 +23,30 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
+
+
+    // owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout:6000,
+        dots: false,
+        nav: true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    //click to croll top
+
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        }, 3000);
+    })
+
+
+    // AOS Instance
+    AOS.init();
 
 });
